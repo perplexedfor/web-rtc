825a74837e1d2308f91e7fc6b694b78007d78bcb
perplexedfor <gusainabhinav037@gmail.com>
2024-07-04 21:38:53 +0530

    single side logic

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, BrowserRouter, Routes} from 'react-router-dom'
+import { Sender } from './components/Sender.tsx'
+import { Receiver } from './components/Receiver.tsx'
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sender" element={<Sender />} />
+        <Route path="/receiver" element={<Receiver />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
